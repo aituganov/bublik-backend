053db086bea19d969b4dbd3d659df12853b6cfc2
Vladislav Aituganov <vaituganov@gmail.com>
2014-07-11 00:56:52 +0600

    add id's to fake companies

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -15,18 +15,21 @@ module UserHelper
 
 	def get_fake_company
 		[{
+			id: 1,
 			title: 'McDonald’s',
 			description: 'крупнейшая в мире сеть ресторанов быстрого питания',
 			rating: 3.89,
 			logoUrl: 'http://bublik.galaxias.co/images/fake_companies/mc.jpg'
 		 },
 		 {
+			id: 2,
 			title: 'IKEA',
 			description: 'Нидерландская компания, владелец одной из крупнейших в мире торговых сетей по продаже мебели и товаров для дома. ',
 			rating: 4.5,
 			logoUrl: 'http://bublik.galaxias.co/images/fake_companies/ikea.png'
 		 },
 		 {
+			id: 3,
 			title: 'Amazon',
 			description: 'Американская компания, крупнейшая в мире по обороту среди продающих товары и услуги через Интернет и один из первых интернет-сервисов, ориентированных на продажу реальных товаров массового спроса',
 			rating: 4.2,
