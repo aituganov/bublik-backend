feceb32bf7433bc257eae950f9162d380bc97832
Vladislav Aituganov <vaituganov@gmail.com>
2014-07-12 23:43:00 +0600

    feat(LocalizationCtrl): replace puts with log4r

--- app/controllers/localization_controller.rb
+++ app/controllers/localization_controller.rb
@@ -10,21 +10,19 @@ class LocalizationController < ApplicationController
 
 	def set_locale
 		accept_language = request.env['HTTP_ACCEPT_LANGUAGE']
-		puts "* Accept-Language: #{accept_language}"
+		logger.info "* Accept-Language value from header: #{accept_language}"
 		@lang = accept_language ? accept_language.scan(/^[a-z]{2}/).first : AppSettings.localization.default_locale
-		if !File.exist?(make_localization_path)
-			puts 'file not found. use default locale'
+		logger.info "Use #{@lang} locale"
+		if !File.exist?(AppSettings.get_path_to_locale(@lang))
+			logger.info 'file not found. use default locale'
 			@lang = AppSettings.localization.default_locale
+		else
+			logger.info 'localization founded!'
 		end
-		puts "* Locale set to '#{@lang}'"
 	end
 
 	def get_localization_contain
-		YAML::load(File.open(make_localization_path))
-	end
-
-	def make_localization_path
-		"#{Rails.root}/#{AppSettings.localization.directory}#{@lang}.yml"
+		YAML::load(File.open(AppSettings.get_path_to_locale(@lang)))
 	end
 
 end
