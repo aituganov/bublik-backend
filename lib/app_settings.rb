7ad37419423fc9bd54a5aa7c9b64e9c295129074
Vladislav Aituganov <vaituganov@gmail.com>
2014-07-12 23:41:31 +0600

    feat(AppSettings): add meted which making the path to localization

--- lib/app_settings.rb
+++ lib/app_settings.rb
@@ -1,4 +1,9 @@
 class AppSettings < Settingslogic
 	source "#{Rails.root}/config/app_settings.yml"
 	namespace Rails.env
+
+	def self.get_path_to_locale(lang)
+		"#{Rails.root}/#{self.localization.directory}#{lang}.yml"
+	end
+
 end
