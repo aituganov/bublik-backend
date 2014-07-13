a9f8c049481c775f0bc379cccb6452457520e54c
Vladislav Aituganov <vaituganov@gmail.com>
2014-07-13 23:05:12 +0600

    fix(WidgetCtrl): change params log palcement

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -4,8 +4,6 @@ class WidgetsController < ApplicationController
 	before_filter :validate_params
 
 	def get
-		logger.info "Getting widget with param: wdidget_id: #{@id}, level: #{@level}, offset: #{@offset}, limit: #{@limit}"
-
 		render json: get_fake_widget(@id, @level, @offset.to_i, @limit.to_i)
 	end
 
@@ -15,6 +13,7 @@ class WidgetsController < ApplicationController
 		@level = params[:level]
 		@offset = params[:offset]
 		@limit = params[:limit] || AppSettings.widgets.def_limit
+		logger.info "Getting widget with param: wdidget_id: #{@id}, level: #{@level}, offset: #{@offset}, limit: #{@limit}"
 		head :bad_request if (@level.nil? || @limit.nil? || @offset.nil?)
 	end
 
