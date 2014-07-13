4e58c0f4a4ea356c7d1cb067db483e2c4b721cb7
Vladislav Aituganov <vaituganov@gmail.com>
2014-07-13 22:39:31 +0600

    test(CompaniesCtrl): create test for CompaniesController

---
+++ spec/controllers/companies_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe CompaniesController do
+
+	context 'GET company info' do
+
+		it 'has a 200 response status code' do
+			get 'get', {id: 1}
+		end
+
+		it 'has a info for requested company' do
+			id = 1
+			get 'get', {id: id}
+			rs_id = JSON.parse(response.body)['id'].to_i
+			rs_id.should eq id
+		end
+
+	end
+
+end
