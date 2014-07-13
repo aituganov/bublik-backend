27e404d73314978e6ffefd4877f4b8438466dff6
Vladislav Aituganov <vaituganov@gmail.com>
2014-07-13 19:36:47 +0600

    feat(appSettings): rename localization default_locale to locale

--- app/controllers/localization_controller.rb
+++ app/controllers/localization_controller.rb
@@ -11,11 +11,11 @@ class LocalizationController < ApplicationController
 	def set_locale
 		accept_language = request.env['HTTP_ACCEPT_LANGUAGE']
 		logger.info "* Accept-Language value from header: #{accept_language}"
-		@lang = accept_language ? accept_language.scan(/^[a-z]{2}/).first : AppSettings.localization.default_locale
+		@lang = accept_language ? accept_language.scan(/^[a-z]{2}/).first : AppSettings.localization.default
 		logger.info "Use #{@lang} locale"
 		if !File.exist?(AppSettings.get_path_to_locale(@lang))
 			logger.info 'file not found. use default locale'
-			@lang = AppSettings.localization.default_locale
+			@lang = AppSettings.localization.default
 		else
 			logger.info 'localization founded!'
 		end
