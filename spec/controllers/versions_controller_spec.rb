03d2bc22a78c19f5bcd83787c9472c970c7390d1
Vladislav Aituganov <vaituganov@gmail.com>
2014-07-17 18:29:50 +0600

    feat(versionsCtrl): add tests for versions controller

---
+++ spec/controllers/versions_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe VersionsController do
+
+	describe 'GET version' do
+		it 'has a 200 status code' do
+			get :current_version
+			response.status.should eq 200
+		end
+
+		it 'has correct version' do
+			version_contain = File.open("#{Rails.root}/version", &:readline)
+			get :current_version
+			response.body.should eq version_contain
+		end
+
+	end
+
+end
