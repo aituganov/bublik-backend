ed03be9f53ceb00d074fa35424d3c702e9523be2
Vladislav Aituganov <vaituganov@gmail.com>
2014-07-19 16:13:48 +0600

    fix(companiesLogo): return random logos from 1 to 10

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -7,7 +7,7 @@ module CompaniesHelper
 			title: "Тарелка #{id}",
 			rating: sprintf('%.2f', rand * 5).to_f,
 			description: 'Служба доставки еды “Тарелка ” – это ваши любимые блюда и напитки в кратчайшие сроки у вас дома, в гостях, в офисе, на даче, и где угодно! В меню вы найдете ассортимент знакомых заведений города: CityBar, Печеная Картошка и CityFood.',
-			fullLogoUrl: "http://bublik.galaxias.co/images/fake_companies/full/#{rand(10)}.jpg",
+			fullLogoUrl: "http://bublik.galaxias.co/images/fake_companies/full/#{1 + rand(9)}.jpg",
 		}
 	end
 
