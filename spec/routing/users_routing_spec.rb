59ed6c6b4153eb9a73107fe5a981c254575928d2
Vladislav Aituganov <vaituganov@gmail.com>
2014-07-21 23:20:07 +0600

    test(usersRoutes): add tests for user routes

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -1,28 +1,34 @@
-# require 'spec_helper'
-#
-# describe UsersController do
-# 	describe 'routing' do
-#
-# 		it 'routes to #index' do
-# 			expect(get: '/api/user').to route_to('users#index')
-# 			# get('/api/user').to route_to('users#index')
-# 		end
-#
-# 		it 'routes to #registration' do
-# 			get('/api/user/new').should route_to('users#registration')
-# 		end
-#
-# 		it 'routes to #login' do
-# 			get('/api/user/login').should route_to('users#login')
-# 		end
-#
-# 		it 'routes to #update' do
-# 			post('/api/user').should route_to('users#update')
-# 			put('/api/user').should route_to('users#update')
-# 		end
-#
-# 		it 'routes to #delete' do
-# 			delete('/api/user').should route_to('users#delete')
-# 		end
-# 	end
-# end
+require 'spec_helper'
+
+describe UsersController do
+	describe 'routing' do
+
+		it 'routes to #index' do
+			get('/api/user').should route_to('users#index')
+		end
+
+		it 'routes to #registration' do
+			put('/api/user/new').should route_to('users#registration')
+		end
+
+		it 'routes to #login' do
+			put('/api/user/login').should route_to('users#login')
+		end
+
+		it 'check empty login route to #page_not_found' do
+			get('/api/user/login/check').should route_to('application#page_not_found', path: 'api/user/login/check')
+		end
+
+		it 'routes to #check_login' do
+			get('/api/user/login/check/new@login').should route_to('users#check_login', login: 'new@login')
+		end
+
+		it 'routes to #update' do
+			post('/api/user').should route_to('users#update')
+		end
+
+		it 'routes to #delete' do
+			delete('/api/user').should route_to('users#delete')
+		end
+	end
+end
