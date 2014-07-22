28152cfcb237e9843051f0ba182937d3498a7157
Vladislav Aituganov <vaituganov@gmail.com>
2014-07-22 19:13:38 +0600

    test(user): add tests for new length validators

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,31 @@ describe User do
 			FactoryGirl.build(:user, login: '').should have(2).error_on(:login)
 		end
 
+		it 'login should error if more then 61' do
+			long_login = (0...30).map { ('a'..'z').to_a[rand(26)] }.join + '@' + (0...27).map { ('a'..'z').to_a[rand(26)] }.join + '.com'
+			FactoryGirl.build(:user, login: long_login).should_not be_valid
+			FactoryGirl.build(:user, login: long_login).should have(1).error_on(:login)
+		end
+
+		it 'should ok if login less or equal then 61' do
+			correct_login = (0...30).map { ('a'..'z').to_a[rand(26)] }.join + '@' + (0...26).map { ('a'..'z').to_a[rand(26)] }.join + '.com'
+			FactoryGirl.build(:user, login: correct_login).should be_valid
+		end
+
 		it 'should require a password' do
 			FactoryGirl.build(:user, password: '').should_not be_valid
 			FactoryGirl.build(:user, password: '').should have(1).error_on(:password)
+		end
+
+		it 'password should error if more then 50' do
+			long_password = (0...51).map { ('a'..'z').to_a[rand(26)] }.join
+			FactoryGirl.build(:user, password: long_password).should_not be_valid
+			FactoryGirl.build(:user, password: long_password).should have(1).error_on(:password)
+		end
+
+		it 'password should ok if less or equal then 50' do
+			correct_password = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+			FactoryGirl.build(:user, password: correct_password).should be_valid
 		end
 
 		it 'should require a first_name' do
@@ -17,10 +39,32 @@ describe User do
 			FactoryGirl.build(:user, first_name: '').should have(1).error_on(:first_name)
 		end
 
+		it 'first_name should error if more then 50' do
+			long_first_name = (0...51).map { ('a'..'z').to_a[rand(26)] }.join
+			FactoryGirl.build(:user, first_name: long_first_name).should_not be_valid
+			FactoryGirl.build(:user, first_name: long_first_name).should have(1).error_on(:first_name)
+		end
+
+		it 'first_name should ok if less or equal then 50' do
+			correct_first_name = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+			FactoryGirl.build(:user, first_name: correct_first_name).should be_valid
+		end
+
 		it 'should require a last_name' do
 			FactoryGirl.build(:user, last_name: '').should_not be_valid
 			FactoryGirl.build(:user, last_name: '').should have(1).error_on(:last_name)
 		end
+	end
+
+	it 'last_name should error if more then 50' do
+		long_last_name = (0...51).map { ('a'..'z').to_a[rand(26)] }.join
+		FactoryGirl.build(:user, last_name: long_last_name).should_not be_valid
+		FactoryGirl.build(:user, last_name: long_last_name).should have(1).error_on(:last_name)
+	end
+
+	it 'last_name should ok if less or equal then 50' do
+		correct_last_name = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+		FactoryGirl.build(:user, last_name: correct_last_name).should be_valid
 	end
 
 	context 'create user' do
