23ce1dbcf65cf2003a2592da1d7606bc4e0b3296
Vladislav Aituganov <vaituganov@gmail.com>
2014-07-27 23:40:18 +0600

    test(user): improve factory structure

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-	factory :user, :class => 'User'  do |u|
+	factory :user, class: 'User'  do |u|
 		u.login      'test@email.ru'
 		u.password   'test_password'
 		u.first_name 'First_name'
 		u.last_name  'Last_name'
 	end
 
-	factory :wrong_user, :class => 'User'  do |u|
+	factory :wrong_user, class: 'User'  do |u|
 		u.login      'wrong_login_format'
 		u.password   'password'
 		u.first_name 'First_name'
