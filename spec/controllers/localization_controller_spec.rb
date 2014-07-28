33937bd65e1c3aff8d44249d2d1fc81617b9fc3d
Vladislav Aituganov <vaituganov@gmail.com>
2014-07-28 14:36:37 +0600

    test(localization): improve test coverage

---
+++ spec/controllers/localization_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe LocalizationController do
+	before :each do
+		@def_locale = AppSettings.localization.default
+	end
+
+	context 'GET localization' do
+		it 'has a 200 status code' do
+			get :get
+			response.status.should eq 200
+		end
+
+		it 'has default localization' do
+			def_contain = YAML::load(File.open(AppSettings.get_path_to_locale(@def_locale))).to_json
+			get :get
+			response.body.should eq def_contain
+		end
+
+		it 'has default localization for unexisted file' do
+			locale = 'kk'
+			def_contain = YAML::load(File.open(AppSettings.get_path_to_locale(@def_locale))).to_json
+			request.headers['HTTP_ACCEPT_LANGUAGE'] = locale
+			get :get
+			response.body.should eq def_contain
+		end
+
+		it 'has defined localization' do
+			locale = 'ru'
+			def_contain = YAML::load(File.open(AppSettings.get_path_to_locale(locale))).to_json
+			request.headers['HTTP_ACCEPT_LANGUAGE'] = locale
+			get :get
+			response.body.should eq def_contain
+		end
+	end
+
+
+end
