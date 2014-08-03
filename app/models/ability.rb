38cac1edd0c4cedcabb1edc20a379d67d27270ac
Vladislav Aituganov <vaituganov@gmail.com>
2014-08-03 18:53:22 +0600

    fix(ability): refactor privileges

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -41,16 +41,14 @@ class Ability
 	end
 
 	def build_privileges(requested_objects)
-		privileges = {:actions => {}}
+		privileges = {actions: {}}
+		actions = privileges[:actions]
 
 		requested_objects.each do |requested|
-			actions = []
 			get_actions.each do |action|
-				actions.push action.to_s if can? action, requested
+				actions[action] = can? action, requested
 			end
-			privileges[:actions][requested.class.name.downcase.to_sym] = actions
 		end
-
 		privileges
 	end
 
