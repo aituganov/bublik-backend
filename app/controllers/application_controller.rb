b120626683a8e6ea16b6493a499f0327b2658656
Vladislav Aituganov <vaituganov@gmail.com>
2014-08-15 21:39:10 +0600

    feat(app): use lograge gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :log_start
 
 	def page_not_found
 		render html: 'Requested resource not found', status: :not_found, layout: false
+	end
+
+	private
+
+	def log_start
+		logger.info "Start #{self.class.name}##{self.action_name} action"
 	end
 
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.lograge.enabled = true
+  config.lograge.custom_options = lambda do |event|
+	  {params: event.payload[:params]}
+  end
 end
